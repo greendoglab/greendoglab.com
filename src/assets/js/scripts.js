0749b55cc3c0bf1319b71a3e7af17ee5fcd1b28f
Michael Yakimenko <macgera@gmail.com>
2015-02-15 14:36:15 +0200

    Equal height.
    – jquery script.

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -1,5 +1,6 @@
 (function($) {
 
+    // reusable functions
     function stateCheck(elem, clName) {
         $el = elem;
         $clName = clName
@@ -10,6 +11,39 @@
         }
     }
 
+    function equalheight(container) {
+
+        var currentTallest = 0,
+        currentRowStart = 0,
+        rowDivs = new Array(),
+        $el,
+        topPosition = 0;
+
+        $(container).each(function() {
+
+            $el = $(this);
+            $($el).height('auto')
+            topPostion = $el.position().top;
+
+            if (currentRowStart != topPostion) {
+                for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                    rowDivs[currentDiv].height(currentTallest);
+                }
+                rowDivs.length = 0; // empty the array
+                currentRowStart = topPostion;
+                currentTallest = $el.height();
+                rowDivs.push($el);
+            } else {
+                rowDivs.push($el);
+                currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+            }
+            for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+                    rowDivs[currentDiv].height(currentTallest);
+            }
+        });
+    }
+
+    // site functions
     function RandomBack() {
         var images = [
             'back2.jpg',
@@ -89,6 +123,7 @@
 
     // document ready
     $(window).on('load', function() {
+        equalheight('.works .work .work-info');
         RandomBack();
         Welcome();
         scrollDownShow();
