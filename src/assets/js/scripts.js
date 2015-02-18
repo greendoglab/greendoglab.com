d8df1e1b743d0a0dfb3ca23e44bf828566e940fb
Michael Yakimenko <macgera@gmail.com>
2015-02-18 05:56:33 +0200

    Change welcome screen images.

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -46,13 +46,13 @@
     // site functions
     function RandomBack() {
         var images = [
-            'back2.jpg',
-            'back4.jpg',
-            'back6.jpg',
-            'back7.jpg',
+            'back3.jpg', // sea
+            'back6.jpg', // sea
+            'back8.jpg', // sea
+            'back9.jpg', // mountians
         ];
 
-        $('.welcome').css({'background-image': 'url(/static/images/' + images[Math.floor(Math.random() * images.length)] + ')'});
+        $('.welcome').css({'background-image': 'url(/static/images/home/' + images[Math.floor(Math.random() * images.length)] + ')'});
     }
 
     function Welcome() {
